c406f93553c65bfdec9ef5cfcf4a6315478ed2cb
Manuel Neves <manuelmneves@hotmail.com>
2022-10-03 16:02:48 +0100

    escreve no final ficheiro

--- Manipula ficheiros/src/manipula/ficheiros/Ficheiro.java
+++ Manipula ficheiros/src/manipula/ficheiros/Ficheiro.java
@@ -1,7 +1,9 @@
 
 package manipula.ficheiros;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -38,7 +40,31 @@ class Ficheiro {
     }
 
     static void escreveNoFinalFicheiro() {
-        
+       String nome;
+        System.out.println("\n\nInsira o nome do ficheiro a editar");        
+        nome = ManipulaFicheiros.ler.nextLine();
+        nome = ManipulaFicheiros.ler.next();
+        File ficheiro = new File(nome);
+        try{
+            if(!ficheiro.exists()){
+                    ficheiro.createNewFile();
+            } 
+            FileWriter fw = new FileWriter(ficheiro,true);
+            BufferedWriter bw = new BufferedWriter(fw);
+            String texto;
+            System.out.println("Insira o conteúdo");
+            texto = ManipulaFicheiros.ler.nextLine();
+            texto = ManipulaFicheiros.ler.nextLine();
+            bw.write(texto);
+            bw.newLine();
+            bw.close();
+            fw.close();
+        }catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        System.out.println("\n"); 
+           
+       
     }
 
     static void escreveFicheiroNovo() {
