1cd3b6165de079587433894616cc4ee0c0d81c9d
Manuel Neves <manuelmneves@hotmail.com>
2022-10-03 14:49:05 +0100

    Ficheiro mais métodos

---
+++ Manipula ficheiros/src/manipula/ficheiros/Ficheiro.java
@@ -0,0 +1,31 @@
+
+package manipula.ficheiros;
+
+
+class Ficheiro {
+
+    static void criaFicheiro() {
+       
+    }
+
+    static void verificaExiste() {
+        
+    }
+
+    static void escreveNoFinalFicheiro() {
+        
+    }
+
+    static void escreveFicheiroNovo() {
+        
+    }
+
+    static void mostraConteudoFicheiro() {
+       
+    }
+
+    static void mostraListaFicheiros() {
+        
+    }
+    
+}
