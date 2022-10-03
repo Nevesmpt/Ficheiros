c37eff6684df05fb4dae4fe8c21040d2afeefdec
Manuel Neves <manuelmneves@hotmail.com>
2022-10-03 14:44:19 +0100

    ficheiros

---
+++ Manipula ficheiros/src/manipula/ficheiros/ManipulaFicheiros.java
@@ -0,0 +1,39 @@
+
+package manipula.ficheiros;
+
+import java.util.Scanner;
+
+
+public class ManipulaFicheiros {
+    
+    public static Scanner ler = new Scanner (System.in);
+
+    
+    public static void main(String[] args) {
+        
+        int op;
+           do{
+            System.out.println("1 - Criar Ficheiro ");
+            System.out.println("2 - Verifica se o ficheiro existe ");
+            System.out.println("3 - Escrever em ficheiro mantendo dados caso exista ");
+            System.out.println("4 - Escrever um novo ficheiro ");
+            System.out.println("5 - Ler um ficheiro ");
+            System.out.println("6 - Mostrar lista de ficheiros ");
+            System.out.println("0 - Sair ");
+            System.out.println(" Seleccione a opção pretendida");
+            switch(op){
+                case 1: Ficheiro.criaFicheiro();break;
+                case 2: Ficheiro.verificaExiste();break;
+                case 3: Ficheiro.escreveNoFinalFicheiro();break;
+                case 4: Ficheiro.escreveFicheiroNovo();break;
+                case 5: Ficheiro.mostraConteudoFicheiro();break;
+                case 6: Ficheiro.mostraListaFicheiros();break;
+                case 0: break;
+                  
+            }
+            
+        }while(op!=0);
+        
+    }
+    
+}
