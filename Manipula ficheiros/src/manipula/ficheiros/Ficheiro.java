cf8f5a80ddaf70438c96527b09da54cfb2a17cc5
Manuel Neves <manuelmneves@hotmail.com>
2022-10-03 14:58:47 +0100

    Implementação mostra lista ficheiros

--- Manipula ficheiros/src/manipula/ficheiros/Ficheiro.java
+++ Manipula ficheiros/src/manipula/ficheiros/Ficheiro.java
@@ -1,6 +1,8 @@
 
 package manipula.ficheiros;
 
+import java.io.File;
+
 
 class Ficheiro {
 
@@ -25,7 +27,12 @@ class Ficheiro {
     }
 
     static void mostraListaFicheiros() {
-        
+        File ficheiro = new File("C:\\Users\\mneves\\Desktop\\Ficheiros\\Ficheiros\\Ficheiros");
+        File [] lista = ficheiro.listFiles();
+        System.out.println(">>>Lista de Ficheiros<<<");
+        for(int x=0;x<lista.length;x++){
+          System.out.println(lista[x].getName());  
+        }
     }
     
 }
