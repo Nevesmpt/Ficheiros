d2853b241a07e10f616b817f0a116c1dbc0e3b50
Manuel Neves <manuelmneves@hotmail.com>
2022-10-03 15:17:43 +0100

    criaFicheiro

--- Manipula ficheiros/src/manipula/ficheiros/Ficheiro.java
+++ Manipula ficheiros/src/manipula/ficheiros/Ficheiro.java
@@ -2,12 +2,25 @@
 package manipula.ficheiros;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 class Ficheiro {
 
     static void criaFicheiro() {
-       
+        String nome;
+       System.out.println("Insira o nome do ficheiro a criar");
+       nome = ManipulaFicheiros.ler.nextLine();
+       File ficheiro = new File(nome);
+       if(!ficheiro.exists()){
+            try {
+                ficheiro.createNewFile();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+       }
     }
 
     static void verificaExiste() {
